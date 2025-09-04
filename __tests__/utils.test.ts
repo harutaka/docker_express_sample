91c6e874015bd30483a6b19dd6c6342a1b9d9f24
harutaka <10650294+harutaka@users.noreply.github.com>
2025-09-04 12:41:41 +0900

    update package version

--- __tests__/utils.test.ts
+++ __tests__/utils.test.ts
@@ -1,4 +1,4 @@
-import { describe, test, expect } from "vitest";
+import { describe, expect, test } from "vitest";
 import { createToken } from "../src/libs/utils.js";
 
 describe("createToken", () => {
